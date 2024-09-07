b3b028ae73218bfa53148056ae9b319e595648d8
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-09-07 01:27:32 -0300

    refactor: main.rs & tests

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,32 @@
-mod suit_context;
-
-use std::io::Error;
-use rustest_core::queue::FIFOQueue;
-use crate::suit_context::SuitContext;
-
-struct FakeTest {
-    cb: Box<dyn Fn()>
-}
-
-impl FakeTest {
-    pub fn run(&self) -> Result<(), Error> {
-       Ok((self.cb)())
-    }
-}
-
 pub fn main() -> () {
     // main
-}
-
-fn execute_suit(mut suit: SuitContext<FakeTest>) -> Result<(), Error> {
-    // runs every before-all-procedure
-    while let Some(bap) = suit.before_all_queue.dequeue() {
-        bap()
-    }
-
-    // runs tests
-    while let Some(test) = suit.tests_queue.dequeue() {
-        let mut before_each_procedures = FIFOQueue::from(suit.before_each_list.clone());
-        let mut after_each_procedures = FIFOQueue::from(suit.after_each_list.clone());
-
-        // runs every before-each-procedure
-        while let Some(bep) = before_each_procedures.dequeue() {
-            bep()
-        }
-
-        test.run()?;
-
-        // runs every after-each-procedure
-        while let Some(aep) = after_each_procedures.dequeue() {
-            aep()
-        }
-    }
-
-    // runs every after-all-procedure
-    while let Some(aap) = suit.after_all_queue.dequeue() {
-        aap()
-    }
-
-    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use std::cell::RefCell;
     use std::rc::Rc;
-    use crate::{execute_suit, FakeTest};
-    use crate::suit_context::SuitContext;
+    use rustest_core::suit_context::SuitContext;
+    use rustest_core::suit_processor::suit_processor;
+    use rustest_core::test::TestContext;
 
     #[test]
     fn engine_flow() {
-        let mut suit: SuitContext<FakeTest> = SuitContext::new();
+        let mut suit: SuitContext = SuitContext::new();
         let outputs: Rc<RefCell<Vec<String>>> = Rc::new(RefCell::new(Vec::new()));
 
         // register tests
         let clone = Rc::clone(&outputs);
-        suit.tests_queue.enqueue(FakeTest {
-            cb: Box::new(move || {
-                clone.borrow_mut().push("Ran call back n° 1!".into());
-            })
-        });
+        suit.tests_queue.enqueue(TestContext::new(Box::new(move || {
+            clone.borrow_mut().push("Ran call back n° 1!".into());
+            Ok(())
+        })));
 
         let clone = Rc::clone(&outputs);
-        suit.tests_queue.enqueue(FakeTest {
-            cb: Box::new(move || {
-                clone.borrow_mut().push("Ran call back n° 2!".into());
-            })
-        });
+        suit.tests_queue.enqueue(TestContext::new(Box::new(move || {
+            clone.borrow_mut().push("Ran call back n° 2!".into());
+            Ok(())
+        })));
 
         // registers before all and after all
         let clone = Rc::clone(&outputs);
@@ -110,8 +61,7 @@ mod tests {
             clone.borrow_mut().push("After each 1 just ran.".into());
         }));
 
-        let result = execute_suit(suit);
-        result.unwrap();
+        let result = suit_processor(suit);
 
         let expected_output: Vec<String> = vec![
             "Before all ran!!!".into(),
@@ -129,5 +79,6 @@ mod tests {
         ];
 
         assert_eq!(expected_output, outputs.take());
+        assert_eq!(0, result.len());
     }
 }
