ec4f2bbf31d1625e185dd855fe0be7679b64b9dc
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-09-07 01:25:43 -0300

    refactor: move suit context to /rustest_core/src/

--- rustest_core/src/lib.rs
+++ rustest_core/src/lib.rs
@@ -1,2 +1,6 @@
-pub mod queue;
 mod extension_traits;
+pub mod queue;
+pub mod suit_context;
+pub mod suit_processor;
+pub mod test;
+pub mod test_error;
--- rustest_core/src/suit_context.rs
+++ rustest_core/src/suit_context.rs
@@ -1,9 +1,8 @@
 use std::rc::Rc;
-use rustest_core::queue::FIFOQueue;
+use crate::queue::FIFOQueue;
+use crate::test::TestContext;
 
-use std::collections::VecDeque;
-
-/// Will be cloned lots of time
+/// Will be cloned lots of time,
 /// so we need it to be a Reference Counter in order of it not going out of scope before being fully used
 type ListProcedure = Rc<dyn Fn()>;
 /// Will be freed as used, so no need to reference-count it, opposed to the `ListProcedure`.
@@ -11,8 +10,7 @@ type QueueProcedure = Box<dyn Fn()>;
 
 /**
 # Suit Context
-A Suit Context represents a situation being tested. It can be declared with the `describe()`
-function.
+A Suit Context represents a situation being tested.
 
 A SuitContext instance is composed by the following elements:
 - **Before All Queue**
@@ -45,15 +43,15 @@ A SuitContext instance is composed by the following elements:
     Context. Each item of the queue will be `dequeue`d (moved) to the
     Execution Stack.
 */
-pub struct SuitContext<T> {
+pub struct SuitContext {
     pub before_all_queue: FIFOQueue<QueueProcedure>,
     pub after_all_queue: FIFOQueue<QueueProcedure>,
     pub before_each_list: Vec<ListProcedure>,
     pub after_each_list: Vec<ListProcedure>,
-    pub tests_queue: FIFOQueue<T>
+    pub tests_queue: FIFOQueue<TestContext>
 }
 
-impl<T> SuitContext<T> {
+impl SuitContext {
     pub fn new() -> Self {
         SuitContext {
             before_all_queue: FIFOQueue::new(),
@@ -65,11 +63,11 @@ impl<T> SuitContext<T> {
     }
 }
 
-impl <T> From<Vec<T>> for SuitContext<T> {
+impl From<Vec<TestContext>> for SuitContext {
     /**
     Instantiates a SuitContext with the given `value` as it's initial `tests_queue`.
     */
-    fn from(value: Vec<T>) -> Self {
+    fn from(value: Vec<TestContext>) -> Self {
         let mut suit = SuitContext::new();
         suit.tests_queue = FIFOQueue::from(value);
         suit
