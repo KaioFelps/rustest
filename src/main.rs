5a0fc5a5217662fe3e722aaa3a86fc5178edc05c
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-09-07 01:08:17 -0300

    test: execution workflow

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,21 @@
 mod suit_context;
 
 use std::io::Error;
-use std::rc::Rc;
 use rustest_core::queue::FIFOQueue;
-use crate::suit_context::{DraftSuitContext, SuitContext};
+use crate::suit_context::SuitContext;
 
-#[derive(Clone)]
 struct FakeTest {
-    cb: Rc<dyn Fn()>
+    cb: Box<dyn Fn()>
 }
 
 impl FakeTest {
     pub fn run(&self) -> Result<(), Error> {
-       Ok(self.cb.clone()())
+       Ok((self.cb)())
     }
 }
 
 pub fn main() -> () {
-    let mut suit_draft: DraftSuitContext<FakeTest> = DraftSuitContext::new();
-    suit_draft.tests_queue.push(FakeTest {
-        cb: Rc::new(|| {
-            println!("Ran call back n° 1!");
-        })
-    });
-
-    let mut suit = SuitContext::from(suit_draft);
-
-    while let Some(test) = suit.tests_queue.dequeue() {
-        let _ = test.run();
-    }
+    // main
 }
 
 fn execute_suit(mut suit: SuitContext<FakeTest>) -> Result<(), Error> {
@@ -65,52 +52,82 @@ fn execute_suit(mut suit: SuitContext<FakeTest>) -> Result<(), Error> {
 
 #[cfg(test)]
 mod tests {
+    use std::cell::RefCell;
     use std::rc::Rc;
     use crate::{execute_suit, FakeTest};
-    use crate::suit_context::DraftSuitContext;
+    use crate::suit_context::SuitContext;
 
     #[test]
     fn engine_flow() {
-        let mut suit_draft: DraftSuitContext<FakeTest> = DraftSuitContext::new();
+        let mut suit: SuitContext<FakeTest> = SuitContext::new();
+        let outputs: Rc<RefCell<Vec<String>>> = Rc::new(RefCell::new(Vec::new()));
 
         // register tests
-        suit_draft.tests_queue.push(FakeTest {
-            cb: Rc::new(|| {
-                println!("Ran call back n° 1!");
+        let clone = Rc::clone(&outputs);
+        suit.tests_queue.enqueue(FakeTest {
+            cb: Box::new(move || {
+                clone.borrow_mut().push("Ran call back n° 1!".into());
             })
         });
 
-        suit_draft.tests_queue.push(FakeTest {
-            cb: Rc::new(|| {
-                println!("Ran call back n° 2!");
+        let clone = Rc::clone(&outputs);
+        suit.tests_queue.enqueue(FakeTest {
+            cb: Box::new(move || {
+                clone.borrow_mut().push("Ran call back n° 2!".into());
             })
         });
 
         // registers before all and after all
-        suit_draft.before_all_queue.push(Rc::new(|| {
-            println!("Before all ran!!!");
+        let clone = Rc::clone(&outputs);
+        suit.before_all_queue.enqueue(Box::new(move || {
+            clone.borrow_mut().push("Before all ran!!!".into());
         }));
-        suit_draft.after_all_queue.push(Rc::new(|| {
-            println!("After all ran!!!");
+
+        let clone = Rc::clone(&outputs);
+        suit.after_all_queue.enqueue(Box::new(move || {
+            clone.borrow_mut().push("After all ran!!!".into());
         }));
 
         // registers before and after each
-        suit_draft.before_each_list.push(Rc::new(|| {
-            println!("Before each 1 just ran.");
-        }));
-        suit_draft.before_each_list.push(Rc::new(|| {
-            println!("Before each 1 just ran.");
-        }));
-        suit_draft.after_each_list.push(Rc::new(|| {
-            println!("After each 1 just ran.");
-        }));
-        suit_draft.after_each_list.push(Rc::new(|| {
-            println!("After each 1 just ran.");
+
+        let clone = Rc::clone(&outputs);
+        suit.before_each_list.push(Rc::new(move || {
+            clone.borrow_mut().push("Before each 1 just ran.".into());
         }));
 
-        let suit = suit_draft.seal();
+        let clone = Rc::clone(&outputs);
+        suit.before_each_list.push(Rc::new(move || {
+            clone.borrow_mut().push("Before each 1 just ran.".into());
+        }));
+
+        let clone = Rc::clone(&outputs);
+        suit.after_each_list.push(Rc::new(move || {
+            clone.borrow_mut().push("After each 1 just ran.".into());
+        }));
+
+        let clone = Rc::clone(&outputs);
+        suit.after_each_list.push(Rc::new(move || {
+            clone.borrow_mut().push("After each 1 just ran.".into());
+        }));
 
         let result = execute_suit(suit);
         result.unwrap();
+
+        let expected_output: Vec<String> = vec![
+            "Before all ran!!!".into(),
+            "Before each 1 just ran.".into(),
+            "Before each 1 just ran.".into(),
+            "Ran call back n° 1!".into(),
+            "After each 1 just ran.".into(),
+            "After each 1 just ran.".into(),
+            "Before each 1 just ran.".into(),
+            "Before each 1 just ran.".into(),
+            "Ran call back n° 2!".into(),
+            "After each 1 just ran.".into(),
+            "After each 1 just ran.".into(),
+            "After all ran!!!".into(),
+        ];
+
+        assert_eq!(expected_output, outputs.take());
     }
 }
