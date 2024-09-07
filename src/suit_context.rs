6e84c2e1522011c4df7394826e59bc6f3a2aff02
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-09-07 01:07:27 -0300

    core: feat: Suit Context Removed draft suit context struct Removed suit context struct's fields getters and made fields public Now SuitContext struct can be instantiated directly (no need to seal a DraftSuitContext to get a SuitContext instance anymore)

--- src/suit_context.rs
+++ src/suit_context.rs
@@ -1,7 +1,13 @@
 use std::rc::Rc;
 use rustest_core::queue::FIFOQueue;
 
-type Procedure = Rc<dyn Fn()>;
+use std::collections::VecDeque;
+
+/// Will be cloned lots of time
+/// so we need it to be a Reference Counter in order of it not going out of scope before being fully used
+type ListProcedure = Rc<dyn Fn()>;
+/// Will be freed as used, so no need to reference-count it, opposed to the `ListProcedure`.
+type QueueProcedure = Box<dyn Fn()>;
 
 /**
 # Suit Context
@@ -39,72 +45,33 @@ A SuitContext instance is composed by the following elements:
     Context. Each item of the queue will be `dequeue`d (moved) to the
     Execution Stack.
 */
-pub struct SuitContext<T: Clone> {
-    pub before_all_queue: FIFOQueue<Procedure>,
-    pub after_all_queue: FIFOQueue<Procedure>,
-    pub before_each_list: Vec<Procedure>,
-    pub after_each_list: Vec<Procedure>,
+pub struct SuitContext<T> {
+    pub before_all_queue: FIFOQueue<QueueProcedure>,
+    pub after_all_queue: FIFOQueue<QueueProcedure>,
+    pub before_each_list: Vec<ListProcedure>,
+    pub after_each_list: Vec<ListProcedure>,
     pub tests_queue: FIFOQueue<T>
 }
 
-impl<T: Clone> From<DraftSuitContext<T>> for SuitContext<T> {
-    /**
-    Converts this DraftSuitContext into a SuitContext.
-
-    It's an alias for `SuitContext::seal(self)` (note that it's a consuming method).
-     */
-    fn from(value: DraftSuitContext<T>) -> Self {
-        value.seal()
-    }
-}
-
-/**
-# Draft Suit Context
-A draft Suit Context is the very first stage of a Suit Context.
-
-A DraftSuitContext instance allows to manipulate every queue and lists as vectors.
-After sealing it (thus getting a SuitContext), queues will behave like queues,
-and lists will be read-only.
-*/
-pub struct DraftSuitContext<T: Clone> {
-    pub before_all_queue: Vec<Procedure>,
-    pub after_all_queue: Vec<Procedure>,
-    pub before_each_list: Vec<Procedure>,
-    pub after_each_list: Vec<Procedure>,
-    pub tests_queue: Vec<T>
-}
-
-impl<T: Clone> DraftSuitContext<T> {
-    /**
-    Instantiates a new `DraftSuitContext`
-
-    **Type Parameters:**
-
-    F:  Type representing the types (that implements Fn trait) of items to
-        be stored in the queues.
-
-    T:  Type of `Test`s that will be stored in the Tests Queue.
-    */
+impl<T> SuitContext<T> {
     pub fn new() -> Self {
-        Self {
-            before_all_queue: Vec::new(),
-            after_all_queue: Vec::new(),
+        SuitContext {
+            before_all_queue: FIFOQueue::new(),
+            after_all_queue: FIFOQueue::new(),
             before_each_list: Vec::new(),
             after_each_list: Vec::new(),
-            tests_queue: Vec::new(),
+            tests_queue: FIFOQueue::new()
         }
     }
+}
 
+impl <T> From<Vec<T>> for SuitContext<T> {
     /**
-    Seals a `DraftSuitContext`, turning it into a `SuitContext`.
+    Instantiates a SuitContext with the given `value` as it's initial `tests_queue`.
     */
-    pub fn seal(self) -> SuitContext<T> {
-        return SuitContext {
-            before_all_queue: FIFOQueue::from(self.before_all_queue),
-            after_all_queue: FIFOQueue::from(self.after_all_queue),
-            before_each_list: self.before_each_list,
-            after_each_list: self.after_each_list,
-            tests_queue: FIFOQueue::from(self.tests_queue),
-        }
+    fn from(value: Vec<T>) -> Self {
+        let mut suit = SuitContext::new();
+        suit.tests_queue = FIFOQueue::from(value);
+        suit
     }
 }
