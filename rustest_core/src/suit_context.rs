8a68c108cee289ba73b185dd6cf8832dbc007acf
Kaio Felipe <kaiofelipe2005@gmail.com>
2024-09-07 01:51:07 -0300

    refactor: core: SuitContext Replaced `Fn` trait by `FnOnce` at QueueProcedure type Rewrote the documentation for Queue and ListProcedure types Replaced `/** */` doc comment by `/// ` doc comments

--- rustest_core/src/suit_context.rs
+++ rustest_core/src/suit_context.rs
@@ -2,47 +2,53 @@ use std::rc::Rc;
 use crate::queue::FIFOQueue;
 use crate::test::TestContext;
 
-/// Will be cloned lots of time,
-/// so we need it to be a Reference Counter in order of it not going out of scope before being fully used
+/// Type of procedures to go inside the Lists of the SuitContext struct
+// Will be cloned lots of times (and thus, called lots of times),
+// so it must be a Reference Counter in order of it not going out of scope before being fully used
+// nor prevented to be called more than once.
 type ListProcedure = Rc<dyn Fn()>;
-/// Will be freed as used, so no need to reference-count it, opposed to the `ListProcedure`.
-type QueueProcedure = Box<dyn Fn()>;
 
-/**
-# Suit Context
-A Suit Context represents a situation being tested.
+/// Type of procedures to go inside the Queues of the SuitContext struct
+// Will be freed as used, so no need to reference-count it, opposed to the ListProcedure.
+// Also, it can be FnOnce (opposed to ListProcedure) as it will be called only once
+// and then dequeued.
+type QueueProcedure = Box<dyn FnOnce()>;
 
-A SuitContext instance is composed by the following elements:
-- **Before All Queue**
-
-    A FIFO queue holding procedures (functions) that must run
-    once at the start of the suit execution (before every test unit).
-    Every item of this list will me **moved** to the Execution Stack
-    before the first, or better, the first *before list *procedure
-    even comes to be executed.
-
-- **After All Queue**
-
-    Just like the *Before All Queue*, but will run at the very end
-    of the suit execution.
-
-- **Before Each List**
-
-    A read-only list of procedures to run before each suit's tests.
-    Every item of this list will be **copied** to the Execution Stack
-    and ran before each test's execution at the Execution Stack.
-
-- **After Each List**
-
-    Just like the *Before Each List*, but holds procedures (functions)
-    to be executed after each test ends running.
-
-- **Testes Queue**
-
-    Finally, this (FIFO) queue holds every test to be run in this Suit
-    Context. Each item of the queue will be `dequeue`d (moved) to the
-    Execution Stack.
-*/
+/// # Suit Context
+/// A Suit Context represents a situation being tested.
+///
+/// A SuitContext instance is composed by the following elements:
+/// - **Before All Queue**
+///
+///     A FIFO queue holding procedures (functions) that must run
+///     once at the start of the suit execution (before every test unit).
+///     Every item of this list will me **moved** to the Execution Stack
+///     before the first, or better, the first *before list *procedure
+///     even comes to be executed.
+///
+/// - **After All Queue**
+///
+///     Just like the *Before All Queue*, but will run at the very end
+///     of the suit execution.
+///
+/// - **Before Each List**
+///
+///     A read-only list of procedures to run before each suit's tests.
+///     Every item of this list will be **copied** to the Execution Stack
+///     and ran before each test's execution at the Execution Stack.
+///
+/// - **After Each List**
+///
+///     Just like the *Before Each List*, but holds procedures (functions)
+///     to be executed after each test ends running.
+///
+/// - **Testes Queue**
+///
+///     Finally, this (FIFO) queue holds every test (of type [`rustest_core::test::TestContext`])
+///     that must be run during this Suit Context execution.
+///     Each item of the queue will be dequeued (moved) to the Execution Stack.
+///
+/// [rustest_core::test::Test]: crate::test::TestContext
 pub struct SuitContext {
     pub before_all_queue: FIFOQueue<QueueProcedure>,
     pub after_all_queue: FIFOQueue<QueueProcedure>,
